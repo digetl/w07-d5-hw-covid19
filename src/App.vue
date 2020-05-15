3d9522ee89f507db8acdb14ca37dfb5f2b46147f
digetl <59475781+digetl@users.noreply.github.com>
2020-05-15 13:58:55 +0100

    Moved folder

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Covid19 Tracker</h1>
+  </div>
+</template>
+
+<script>
+import World from './components/World.vue'
+
+export default {
+  name: 'App',
+  data(){
+    return{
+      worlddata:{}
+    }
+  },
+  methods: {
+
+  },
+
+  mounted(){
+    fetch("https://api.covid19api.com/summary")
+    .then(res => res.json())
+    .then(data => this.worlddata = data)
+    .catch(error => console.log("error:", error))
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/World.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>World data></h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
