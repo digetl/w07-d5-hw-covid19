38c5afdb85439f5993a9948fe7cadc361df5fb04
digetl <59475781+digetl@users.noreply.github.com>
2020-05-15 17:00:15 +0100

    Global data displayed

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,18 @@
   <div id="app">
     <h1>Covid19 Tracker</h1>
     <world-component></world-component>
+    <div class="container">
+      <h1>World data</h1>
+        <ul>
+          <li>New Confirmed Cases: {{globalData.NewConfirmed}}</li>
+          <li>New Deaths: {{globalData.NewDeaths}}</li>
+          <li>New Recovered: {{globalData.NewRecovered}}</li>
+          <li>Total Confirmed Cases: {{globalData.TotalConfirmed}}</li>
+          <li>Total Deaths: {{globalData.TotalDeaths}}</li>
+          <li>Total Recovered: {{globalData.TotalRecovered}}</li>
+
+        </ul>
+    </div>
   </div>
 </template>
 
@@ -39,6 +51,7 @@ export default {
 
 <style>
 #app {
+  padding:0 20%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -46,4 +59,16 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.container{
+  padding: auto;
+}
+
+ul {
+  list-style: none;
+  padding-inline-start: 0px;
+  line-height: 2rem;
+
+}
+
 </style>
--- src/components/World.vue
+++ src/components/World.vue
@@ -2,15 +2,33 @@
 <div>
 
 <h1>World data</h1>
-
-<p></p>
-
+  <ul>
+    <li>
+     Text: {{globalData.NewConfirmed}} 
+    </li>
+  </ul>
 </div>
   
 </template>
 
 <script>
+import { eventBus } from '../main.js'
+
+
 export default {
+  name: "world-component",
+  props: {
+    // NewConfirmed: Number,
+    // NewDeaths: Number,
+    // NewRecovered: Number,
+    // TotalConfirmed: Number,
+    // TotalDeaths: Number,
+    // TotalRecoverd: Number
+    
+  },
+  methods: {
+    
+  }
 
 }
 </script>
